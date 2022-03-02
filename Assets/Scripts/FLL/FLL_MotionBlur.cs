555cfc62a14f7b88c236bbb698c3d4c3ea2125b0
shi-yh <syhxy008@qq.com>
2022-03-02 11:28:45 +0800

    运功模糊

---
+++ Assets/Scripts/FLL/FLL_MotionBlur.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FLL_MotionBlur : FLL_PostEffetctBase
+{
+    public Shader motionBlurShader;
+
+    private Material motionBlurMaterial = null;
+
+    public Material material
+    {
+        get
+        {
+            motionBlurMaterial = CheckShaderAndCreateMaterial(motionBlurShader, motionBlurMaterial);
+            return motionBlurMaterial;
+        }
+    }
+
+    private RenderTexture accmulationTexture;
+
+    [Range(0.0f, 0.9f)] public float blurAmount = 0.5f;
+
+
+    private void OnDisable()
+    {
+        DestroyImmediate(accmulationTexture);
+    }
+
+    private void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        if (material != null)
+        {
+            if (accmulationTexture == null || accmulationTexture.width != src.width || accmulationTexture.height != src.height)
+            {
+                DestroyImmediate(accmulationTexture);
+
+                accmulationTexture = new RenderTexture(src.width, src.height, 0);
+
+                accmulationTexture.hideFlags = HideFlags.HideAndDontSave;
+
+                Graphics.Blit(src, accmulationTexture);
+            }
+            
+
+            material.SetFloat("_BlurAmount", 1.0f - blurAmount);
+            
+            ///不是特别理解，accmulationTexture好像没有用到？是因为accmulationTexture函数obsolete了？
+            Graphics.Blit(src,accmulationTexture,material);
+            Graphics.Blit(accmulationTexture,dest);
+            
+        }
+        else
+        {
+            Graphics.Blit(src,dest);
+        }
+    }
+}
---
+++ Assets/Shaders/FLL/FLL_MotionBlur.shader
@@ -0,0 +1,84 @@
+Shader "FLL/FLL_MotionBlur"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+        _BlurAmount("Blur Amount",Float) = 1.0
+    }
+    SubShader
+    {
+        Tags
+        {
+            "RenderType"="Opaque"
+        }
+        LOD 100
+
+        CGINCLUDE
+        #include "UnityCG.cginc"
+
+        sampler2D _MainTex;
+        float4 _MainTex_ST;
+        fixed _BlurAmount;
+
+        struct v2f
+        {
+            float4 pos:SV_POSITION;
+
+            half2 uv:TEXCOORD0;
+        };
+
+        v2f vert(appdata_img v)
+        {
+            v2f o;
+
+            o.pos = UnityObjectToClipPos(v.vertex);
+
+            o.uv = v.texcoord;
+
+            return o;
+        }
+
+        fixed4 fragRGB(v2f i):SV_Target
+        {
+            return fixed4(tex2D(_MainTex, i.uv).rgb, _BlurAmount);
+        }
+
+        half4 fragA(v2f i):SV_Target
+        {
+            return tex2D(_MainTex, i.uv);
+        }
+        ENDCG
+
+        ZTest Always
+        Cull Off
+        ZWrite Off
+
+        Pass
+        {
+            Blend SrcAlpha OneMinusSrcAlpha
+
+            ColorMask RGB
+
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment fragRGB
+            ENDCG
+        }
+
+        Pass
+        {
+            Blend One Zero
+            ColorMask A
+            CGPROGRAM
+
+
+            #pragma vertex vert
+            #pragma fragment fragA
+            
+            ENDCG
+            
+        }
+
+    }
+    Fallback Off
+}
